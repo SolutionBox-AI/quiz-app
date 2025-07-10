945c1f6245b0aaf8fddb32e9772468c20f1a5dbe
SolutionBox-AI <abhishekbhardwaj7819@gmail.com>
2025-07-10 16:31:35 +0530

    Update Question.js

--- backend/models/Question.js
+++ backend/models/Question.js
@@ -1,1 +1,15 @@
+const mongoose = require('mongoose');
+
+const optionSchema = new mongoose.Schema({
+  label: String,
+  text: String
+});
+
+const questionSchema = new mongoose.Schema({
+  testId: String,
+  question: String,
+  options: [optionSchema]
+});
+
+module.exports = mongoose.model('Question', questionSchema);
 
