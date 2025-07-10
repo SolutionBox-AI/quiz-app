dfef9e5c36fd02d82ce99fbee9b3b7e4572841db
SolutionBox-AI <abhishekbhardwaj7819@gmail.com>
2025-07-10 22:41:33 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,10 @@ const mongoose = require('mongoose');
 
 const app = express();
 
+
+console.log("Connecting to MongoDB URI:", process.env.MONGODB_URI);
+
+
 // Middlewares
 app.use(cors());
 app.use(express.json());
