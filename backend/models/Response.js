1beb854bc6256b76afff44425147067e1dc58eff
SolutionBox-AI <abhishekbhardwaj7819@gmail.com>
2025-07-10 16:32:33 +0530

    Update Response.js

--- backend/models/Response.js
+++ backend/models/Response.js
@@ -1,1 +1,13 @@
+const mongoose = require('mongoose');
+
+const responseSchema = new mongoose.Schema({
+  name: String,
+  town: String,
+  code: String,
+  testId: String,
+  answers: [String],
+  submittedAt: Date
+});
+
+module.exports = mongoose.model('Response', responseSchema);
 
