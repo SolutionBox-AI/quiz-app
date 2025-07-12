a80231c8ee72044be3ecf67e05fb4e906ab2d5b6
SolutionBox-AI <abhishekbhardwaj7819@gmail.com>
2025-07-12 12:05:35 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -14,6 +14,11 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.get('/', (req, res) => {
+  res.send('Quiz Backend is Online');
+});
+
+
 // Routes
 app.use('/api/quiz', quizRoutes);
 
