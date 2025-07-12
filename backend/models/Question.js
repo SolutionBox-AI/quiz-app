07d73b81930592ce5f722cdd2f63c5b02be346c7
SolutionBox-AI <abhishekbhardwaj7819@gmail.com>
2025-07-12 13:09:56 +0530

    Update Question.js

--- backend/models/Question.js
+++ backend/models/Question.js
@@ -1,10 +1,13 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const questionSchema = new mongoose.Schema({
-  testId: String,
-  question: String,
-  options: [String],
-  correctAnswer: String
+  testId: { type: String, required: true },
+  question: { type: String, required: true },
+  options: {
+    type: [String],
+    validate: v => Array.isArray(v) && v.length === 4
+  },
+  correctAnswer: { type: String, required: true }
 });
 
-module.exports = mongoose.model('Question', questionSchema);
+module.exports = mongoose.model("Question", questionSchema);
