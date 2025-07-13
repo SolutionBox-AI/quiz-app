3761a9c122d862e8fd645485bfb3be5432b6dd73
SolutionBox-AI <abhishekbhardwaj7819@gmail.com>
2025-07-13 21:33:18 +0530

    Update AdminStudentMap.js

--- backend/models/AdminStudentMap.js
+++ backend/models/AdminStudentMap.js
@@ -1,1 +1,11 @@
+const mongoose = require("mongoose");
+
+const AdminStudentMapSchema = new mongoose.Schema({
+  adminEmail: { type: String, required: true },
+  adminName: { type: String, required: true },
+  studentCode: { type: String, required: true },
+  studentName: { type: String, required: true }
+});
+
+module.exports = mongoose.model("AdminStudentMap", AdminStudentMapSchema);
 
