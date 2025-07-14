fe8f22084608eca9ce682ef20f4020b46d3b1f98
SolutionBox-AI <abhishekbhardwaj7819@gmail.com>
2025-07-14 12:32:13 +0530

    Update mapping.js

--- backend/models/mapping.js
+++ backend/models/mapping.js
@@ -1,1 +1,12 @@
+// models/Mapping.js
+const mongoose = require("mongoose");
+
+const mappingSchema = new mongoose.Schema({
+  adminCode: String,
+  studentCode: String,
+  adminName: String,
+  studentName: String
+});
+
+module.exports = mongoose.model("Mapping", mappingSchema);
 
