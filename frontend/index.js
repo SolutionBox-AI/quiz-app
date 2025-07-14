7ab5b89987e3599a81e461a57053c861791a7a0a
SolutionBox-AI <abhishekbhardwaj7819@gmail.com>
2025-07-14 18:24:30 +0530

    Update index.js

--- frontend/index.js
+++ frontend/index.js
@@ -45,7 +45,7 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.listen(3000, () => console.log('Server running on http://localhost:3000'));
+app.listen(3000, () => console.log('Server running on http://localhost:10000'));
 
 
 
